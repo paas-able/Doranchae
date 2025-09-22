103714aa8298f94e9d2496379d94ec5f49c24ef8
jayde1012 <ariha1982@naver.com>
2025-09-22 16:12:21 +0900

    색상코드 및 글꼴 설정

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -5,7 +5,32 @@ module.exports = {
       "./src/components/**/*.{ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            green1: "#8B9744",
+            green2: "#CED5B2",
+            green3: "#EAEDCC",
+            brown1: "#CCA57A",
+            brown2: "#F8EDD0",
+            brown3: "#FDFAE3",
+            background: "#FDFAED",
+            alert: "#DE735B",
+
+            gray1: "#4D4D4D",
+            gray2: "#666666",
+            gray3: "#808080",
+            gray4: "#999999",
+            gray5: "#B3B3B3",
+            gray6: "#CCCCCC",
+            gray7: "#E6E6E6",
+            gray8: "#E6E6E6"
+        },
+
+        fontFamily: {
+            sans: ['Pretendard', 'ui-sans-serif', 'system-ui'],
+            display: ['Pretendard', 'ui-sans-serif', 'system-ui'],
+        }
+    },
   },
   plugins: [],
 }
