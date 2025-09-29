ba18a8affc83a703c98282c1819f6bbea496fc0c
Jayde1012 <ariha1982@naver.com>
2025-09-29 19:23:05 +0900

    [FEAT] 상단바

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -11,7 +11,7 @@ import SearchIcon from '@mui/icons-material/Search';
 import logo from '@assets/drch_logo.png';
 
 const Header: React.FC = () => {
-    const { pathname } = useRouter()
+    // const { pathname } = useRouter()
 
     return (
         <div className="w-full h-fit justify-between flex items-center px-[20px] py-[15px]">
