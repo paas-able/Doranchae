727a9c7827cec06db74558b018ae6bfbc5289b8f
jayde1012 <ariha1982@naver.com>
2025-09-30 12:07:16 +0900

    pathname 에러 수정, 이음이 채팅 페이지 연결

---
+++ app/src/app/ieumi/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from 'react';
+
+const IeumiPage = () => {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center p-24">
+            <h1 className="text-4xl font-bold text-gray-800 mb-4">
+                이음이 채팅 페이지
+            </h1>
+            <p className="text-lg text-gray-600">
+                여기는 이음이와 채팅을 하는 공간입니다.
+            </p>
+        </div>
+    );
+};
+
+export default IeumiPage;
--- app/src/components/NavBar.tsx
+++ app/src/components/NavBar.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import Link from "next/link";
-import {useRouter} from "next/navigation";
+import {usePathname} from "next/navigation";
 import {SvgIcon} from "@mui/material";
 import InfoOutlineRoundedIcon from '@mui/icons-material/InfoOutlineRounded';
 import InfoRoundedIcon from '@mui/icons-material/InfoRounded';
@@ -16,8 +16,7 @@ import Image from 'next/image'
 import ieumi from '../../public/ieumi.png'
 
 const BottomNav: React.FC = () => {
-    // const { pathname } = useRouter()
-    // 주석 추가
+    const pathname = usePathname()
 
     return (
         <div className="fixed bottom-0 w-full h-fit">
@@ -25,28 +24,28 @@ const BottomNav: React.FC = () => {
                 {/*왼쪽 아이콘 묶음*/}
                 <div className="flex justify-between w-[125px]">
                     <Link href="/welfare" className="flex flex-col items-center">
-                        <SvgIcon className="" component={ pathname === 'welfare' ? InfoRoundedIcon : InfoOutlineRoundedIcon} sx={{ fontSize: 30 }}/>
+                        <SvgIcon className="" component={ pathname === '/welfare' ? InfoRoundedIcon : InfoOutlineRoundedIcon} sx={{ fontSize: 30 }}/>
                         <label className="block font-semibold">복지 정보</label>
                     </Link>
                     <Link href="/penpal" className="flex flex-col items-center">
-                        <SvgIcon className="" component={ pathname === 'penpal' ? EmailIcon : EmailOutlinedIcon } sx={{ fontSize: 30 }}/>
+                        <SvgIcon className="" component={ pathname === '/penpal' ? EmailIcon : EmailOutlinedIcon } sx={{ fontSize: 30 }}/>
                         <label className="block font-semibold">펜팔</label>
                     </Link>
                 </div>
 
                 {/*가운데 아이콘*/}
-                <div className="bg-brown2 border-2 border-green1 w-[80px] h-[80px] rounded-full mb-8 flex justify-center items-center">
+                <Link href="/ieumi" className="bg-brown2 border-2 border-green1 w-[80px] h-[80px] rounded-full mb-8 flex justify-center items-center">
                     <Image src={ieumi} alt="AI Chat Button" width={45} height={60}/>
-                </div>
+                </Link>
 
                 {/*오른쪽 아이콘 묶음*/}
                 <div className="flex justify-between w-[125px]">
                     <Link href="/community" className="flex flex-col items-center">
-                        <SvgIcon className="" component={ pathname === 'community' ? ArticleIcon : ArticleOutlinedIcon} sx={{ fontSize: 30 }}/>
+                        <SvgIcon className="" component={ pathname === '/community' ? ArticleIcon : ArticleOutlinedIcon} sx={{ fontSize: 30 }}/>
                         <label className="block font-semibold">커뮤니티</label>
                     </Link>
                     <Link href="/mypage" className="flex flex-col items-center">
-                        <SvgIcon className="" component={ pathname === 'mypage' ? AccountCircleIcon : AccountCircleOutlinedIcon } sx={{ fontSize: 30 }}/>
+                        <SvgIcon className="" component={ pathname === '/mypage' ? AccountCircleIcon : AccountCircleOutlinedIcon } sx={{ fontSize: 30 }}/>
                         <label className="block font-semibold">내 정보</label>
                     </Link>
                 </div>
