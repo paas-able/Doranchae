7bf76016fbe156ec5ebcf859cea437557506213f
jayde1012 <ariha1982@naver.com>
2025-10-02 18:21:42 +0900

    복지정보 리스트 컴포넌트, layout.tsx children margin 적용

--- app/src/app/welfare/page.tsx
+++ app/src/app/welfare/page.tsx
@@ -1,16 +1,23 @@
 "use client";
 
 import React from 'react';
+import WelfareElement from "@components/WelfareElement";
 
 const WelfarePage = () => {
     return (
-        <div className="flex min-h-screen flex-col items-center justify-center p-24">
+        <div className="flex h-full flex-col justify-center overflow-clip">
             <h1 className="text-4xl font-bold text-gray-800 mb-4">
                 복지 정보 페이지
             </h1>
-            <p className="text-lg text-gray-600">
-                여기는 복지 관련 정보가 표시되는 공간입니다.
-            </p>
+            <div className={'h-full overflow-scroll'}>
+                <WelfareElement id={1} title={'title'} region={'region'} image={'url'} date={'2025-12-13'} tag={'tag'} likes={13} isScrap={false}/>
+                <WelfareElement id={1} title={'title'} region={'region'} image={'url'} date={'2025-12-13'} tag={'tag'} likes={13} isScrap={true}/>
+                <WelfareElement id={1} title={'title'} region={'region'} image={'url'} date={'2025-12-13'} tag={'tag'} likes={13} isScrap={true}/>
+                <WelfareElement id={1} title={'title'} region={'region'} image={'url'} date={'2025-12-13'} tag={'tag'} likes={13} isScrap={true}/>
+                <WelfareElement id={1} title={'title'} region={'region'} image={'url'} date={'2025-12-13'} tag={'tag'} likes={13} isScrap={true}/>
+                <WelfareElement id={1} title={'title'} region={'region'} image={'url'} date={'2025-12-13'} tag={'tag'} likes={13} isScrap={true}/>
+                <WelfareElement id={1} title={'title'} region={'region'} image={'url'} date={'2025-12-13'} tag={'tag'} likes={13} isScrap={true}/>
+            </div>
         </div>
     );
 };
