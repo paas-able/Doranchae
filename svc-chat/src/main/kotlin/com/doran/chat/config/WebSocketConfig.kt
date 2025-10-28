93b0229c4537beea36b937806c6eb585e0732856
인다운 <incrying@duksung.ac.kr>
2025-10-28 16:37:03 +0900

    [FIX] taskSchedular 필드 주입으로 변경

--- svc-chat/src/main/kotlin/com/doran/chat/config/WebSocketConfig.kt
+++ svc-chat/src/main/kotlin/com/doran/chat/config/WebSocketConfig.kt
@@ -1,7 +1,9 @@
 package com.doran.chat.config
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.scheduling.TaskScheduler
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
@@ -9,6 +11,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 class WebSocketConfig : WebSocketMessageBrokerConfigurer {
+    @Autowired
+    private lateinit var taskScheduler: TaskScheduler
+
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
         registry.addEndpoint("/ws-chat")
             .setAllowedOriginPatterns("*")
@@ -17,6 +22,7 @@ class WebSocketConfig : WebSocketMessageBrokerConfigurer {
 
     override fun configureMessageBroker(registry: MessageBrokerRegistry) {
         registry.enableSimpleBroker("/topic")
+            .setTaskScheduler(taskScheduler)
             .setHeartbeatValue(longArrayOf(10_000, 20_000))
 
         registry.setApplicationDestinationPrefixes("/app")
